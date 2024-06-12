32572ec9fb5a47354dfe7a710dfd3285f91360df
maltegtr <malte.grueter24@gmail.com>
2024-06-12 18:10:49 +0200

    Auswertung fertiggestellt

--- Wetterdatenauswertung/Form1.cs
+++ Wetterdatenauswertung/Form1.cs
@@ -16,6 +16,15 @@ namespace Wetterdatenauswertung
         string filename = @"..\..\dateien\wetterdaten.csv";
         //string inporttestfilename = @"..\..\dateien\test_wetterdaten.csv";
         //string exporttestfilename = @"..\..\dateien\wetterdaten_export.csv";
+        /*double durchschnittstemperatur;
+        double durchschnittsfeuchtigkeit;
+        double maximaltemperatur;
+        double minimaltemperatur;
+        int temperatur0_10;
+        int temperatur10_20;
+        int temperatur20_30;
+        int feuchtigkeit80;
+        */
 
 
         List<wetterdatum> wetterdatenliste = new List<wetterdatum>();
@@ -24,6 +33,7 @@ namespace Wetterdatenauswertung
         public Form1()
         {
             InitializeComponent();
+            lblAuswertung.Visible = false;
             lstBoxAusgabe.Items.Add($"Datum - Temperatur[°C] - Feuchtigkeit[%]");
         }
 
@@ -43,11 +53,23 @@ namespace Wetterdatenauswertung
 
         private void btnSpeichernListe_Click(object sender, EventArgs e)
         {
-            wetterdatum neuesWetter = new wetterdatum(dateTimePicker1.Value,double.Parse(txtBoxTemperatur.Text), double.Parse(txtBoxFeuchtigkeit.Text));
+            wetterdatum neuesWetter = new wetterdatum(dateTimePicker1.Value, double.Parse(txtBoxTemperatur.Text), double.Parse(txtBoxFeuchtigkeit.Text));
             wetterdatenliste.Add(neuesWetter);
             lstBoxAusgabe.Items.Add(neuesWetter.ToString());
         }
-
+        private void btnAuswertung_Click(object sender, EventArgs e)
+        {
+            lblAuswertung.Visible = true;
+            lblAuswertung.Text = null;
+            lblAuswertung.Text += $"Durchschnittstemperatur: {Durchschnittstemperatur().ToString("##.##")}°C\r\n";
+            lblAuswertung.Text += $"Durchschnittsfeuchtigkeit:{Durchschnittsfeuchtigkeit().ToString("##.##")}%\r\n";
+            lblAuswertung.Text += $"Maximaltemperatur: {Maximaltemperatur().ToString("##.##")}°C\r\n";
+            lblAuswertung.Text += $"Minimaltemperatur: {Minimaltemperatur().ToString("##.##")}°C\r\n";
+            lblAuswertung.Text += $"\r\n" + $"0-10°C: {Temperatur_0_10()} Tage\r\n";
+            lblAuswertung.Text += $"10-20°C: {Temperatur_10_20()} Tage\r\n";
+            lblAuswertung.Text += $"20-30°C: {Temperatur_20_30()} Tage\r\n";
+            lblAuswertung.Text += $"\r\nTage mit Luftfeuchtigkeit >79%: {Feuchtigkeit_80()} Tage\r\n";
+        }
         private void Import()
         {
             int i = 0;
@@ -82,7 +104,7 @@ namespace Wetterdatenauswertung
                 {
                     if (i == 0)
                     {
-                        myFile.WriteLine("Timestamp;Temperature (Celsius);Humidity (%)"); 
+                        myFile.WriteLine("Timestamp;Temperature (Celsius);Humidity (%)");
                         i++;
                     }
 
@@ -97,6 +119,98 @@ namespace Wetterdatenauswertung
             }
             else MessageBox.Show($"Datei nicht gefunden!");
         }
+        private double Durchschnittstemperatur()
+        {
+            double durschnittstemperatur = 0;
+            int i = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                durschnittstemperatur += content.Temperatur;
+                i++;
+            }
+            durschnittstemperatur = durschnittstemperatur / i;
+            return durschnittstemperatur;
+        }
+        private double Durchschnittsfeuchtigkeit()
+        {
+            double durchschnittsfeuchtigkeit = 0;
+            int i = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                durchschnittsfeuchtigkeit += content.Feuchtigkeit;
+                i++;
+            }
+            durchschnittsfeuchtigkeit = durchschnittsfeuchtigkeit / i;
+            return durchschnittsfeuchtigkeit;
+        }
 
+        private double Maximaltemperatur()
+        {
+            double maximaltemperatur = 0;
+            int i = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (i != 0)
+                {
+                    maximaltemperatur = content.Temperatur;
+                    i++;
+                }
+                else if (content.Temperatur > maximaltemperatur) maximaltemperatur = content.Temperatur;
+            }
+            return maximaltemperatur;
+        }
+        private double Minimaltemperatur()
+        {
+            double minimaltemperatur = 0;
+            int i = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (i != 0)
+                {
+                    minimaltemperatur = content.Temperatur;
+                    i++;
+                }
+                else if (content.Temperatur < minimaltemperatur) minimaltemperatur = content.Temperatur;
+            }
+            return minimaltemperatur;
+        }
+        private int Temperatur_0_10()
+        {
+            int tage = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (content.Temperatur >= 0 && content.Temperatur <= 10) tage++;
+            }
+            return tage;
+        }
+        private int Temperatur_10_20()
+        {
+            int tage = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (content.Temperatur >= 10 && content.Temperatur <= 20) tage++;
+            }
+            return tage;
+        }
+
+        private int Temperatur_20_30()
+        {
+            int tage = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (content.Temperatur >= 20 && content.Temperatur <= 30) tage++;
+            }
+            return tage;
+        }
+
+        private int Feuchtigkeit_80()
+        {
+            int tage = 0;
+            foreach (var content in wetterdatenliste)
+            {
+                if (content.Feuchtigkeit > 79) tage++;
+            }
+            return tage;
+        }
     }
 }
